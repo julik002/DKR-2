8bfad12c565ce0057705a5f73835218e2413d575
julik002 <117649745+julik002@users.noreply.github.com>
2022-11-21 09:37:41 +0300

    Add files via upload

---
+++ 2 кр/задание 1.pas
@@ -0,0 +1,24 @@
+﻿begin
+var a:array[1..5] of integer;
+println('введите массив:');
+begin 
+for var i:=1 to 5 do 
+a[i]:=ReadInteger;
+end;
+
+ var k:=ReadInteger('введите k:');
+
+ var u:=1;
+ 
+   while u<=k do
+      begin
+     var c:=5;
+      for var i:=1 to 5 do
+   begin
+     if a[i]<>u then
+   c:=c-1;
+    end;
+   if c=0 then print(' не используются:',u);
+   u:=u+1;
+   end;
+end.
---
+++ 2 кр/задание 2.pas
@@ -0,0 +1,20 @@
+﻿begin
+var a:=ReadString('Введите строку:');
+var b:=ReadString('Введите подстроку:');
+var s:integer;
+var x:string;
+var i:=1;
+for var n:=1 to length(b) do
+a:=a+' ';
+while i<length(a) do begin
+for var j:=1 to length(b) do begin
+if a[i+j-1]=b[j] then s:=1 else begin s:=0; break; end;
+end;
+if s=1 then for var m:=length(b) downto 1 do begin x:=x+b[m]; i:=i+1;
+end
+else begin x:=x+a[i]; i:=i+1;
+end;
+end;
+println(x);
+end.
+
